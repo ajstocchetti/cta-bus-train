8f0d0998d78718794e7810c4366677488dd8be8e
tackypinkflamingos <ajstocchetti@gmail.com>
2019-02-23 18:35:46 -0600

    add toggle buttons

---
+++ public/app.js
@@ -0,0 +1,148 @@
+window.onload = function() {
+  // setup click handler for control pannel
+  window.cta_controller = {
+    autorefresh: true,
+    autopan: true,
+  };
+
+
+  // initialize click handlers
+  $('#ctrl-resize').click(function() {
+    $("#ctrl-items").toggleClass('hide');
+  });
+  $('#refreshcheck').click(function() {
+    window.cta_controller.autorefresh = $('#refreshcheck').is(':checked');
+  });
+  $('#scrollcheck').click(function() {
+    window.cta_controller.autopan = $('#scrollcheck').is(':checked');
+  });
+
+  window.cta_config = window.cta_config || {};
+  if (!Array.isArray(window.cta_config.busses)) window.cta_config.busses = [];
+  initMap(window.cta_config.home);
+
+  setInterval(updateBusses, 150000);
+  updateBusses();
+}
+
+function initMap(homeCoords) {
+  var center = homeCoords || [41.8781, -87.6298]; // Chicago
+  map = L.map('map', {
+    center: center,
+    zoom: 16
+  });
+  window.cta_config.map = map;
+
+  var Esri_WorldStreetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+  });
+  Esri_WorldStreetMap.addTo(map);
+
+  if (homeCoords) {
+    var home = L.marker(homeCoords, {
+      // title: 'home',
+      riseOnHover: true,
+      icon: L.icon({
+        iconUrl: './home-icon.png',
+        iconSize: [30, 37],
+        iconAnchor: [15, 30],
+      })
+    });
+    home.on('mouseover', function(e) {
+      L.popup().setLatLng(e.latlng).setContent('Home').openOn(map);
+    }).addTo(map);
+    window.cta_config.home_marker = home;
+  }
+}
+
+
+
+
+  // https://leaflet-extras.github.io/leaflet-providers/preview/
+  // var Hydda_Full = L.tileLayer('https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', {
+  // 	maxZoom: 18,
+  // 	attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  // });
+  // var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+  // 	maxZoom: 18,
+  // 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  // });
+  // var OpenStreetMap_DE = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+  // 	maxZoom: 18,
+  // 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  // });
+  // var Stamen_Terrain = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
+  // 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  // 	subdomains: 'abcd',
+  // 	minZoom: 0,
+  // 	maxZoom: 18,
+  // 	ext: 'png'
+  // });
+  // var Esri_WorldStreetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+  // 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+  // });
+  // var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+  // 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+  // });
+  // var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+  // 	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+  // 	maxZoom: 16
+  // });
+
+
+function updateBusses() {
+  console.log('updating busses');
+  var map = window.cta_config.map;
+  var busses = window.cta_config.busses;
+  var busStops = window.cta_config.busStops || [];
+  $.ajax({
+    url: `/api/bus?stops=${busStops}`,
+    context: document.body
+  })
+  .done(function(resp) {
+    busses.forEach(function(marker) {
+      map.removeLayer(marker);
+    });
+    busses.length = 0;
+    Object.keys(resp).forEach(stopId => {
+      resp[stopId].forEach(veh => addToBusses(veh));
+    });
+
+    // adjust zoom
+    var fg = [...busses];
+    if (window.cta_config.home_marker) fg.push(window.cta_config.home_marker);
+    if (window.cta_controller.autopan) map.fitBounds(L.featureGroup(fg).getBounds());
+  })
+  .error(function(req, status, err) {
+    console.log('error in ajax');
+    console.log(status);
+    console.log(err);
+  });
+}
+
+function addToBusses(vpData) {
+  var vehicle = vpData.veh;
+  var pred = vpData.pred;
+  var map = window.cta_config.map;
+  var angle = (Math.round(vehicle.hdg / 5) * 5) % 360; // wrap 360 back to 0
+  var marker = L.marker([ vehicle.lat, vehicle.lon], {
+    icon: L.icon({
+      iconUrl: `/icons/bus-${angle}.png`
+    })
+  });
+  var text = `Route ${vehicle.rt} to ${pred.stpnm} (${pred.rtdir})`;
+  if (vehicle.dly) text += 'Vehicle is delayed.';
+  text += `Arrives at ${pred.ts}`;
+  text += `.  Heading: ${vehicle.hdg}`
+
+  marker.on('mouseover', function(e) {
+    //open popup;
+    var popup = L.popup()
+     .setLatLng(e.latlng)
+     .setContent(text)
+     .openOn(map);
+  });
+
+  window.cta_config.busses.push(marker);
+  marker.addTo(map);
+}
